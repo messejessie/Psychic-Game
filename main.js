62a3be71d2e11fb5884a302d20565057070ea1e9
messejessie <messejessie77@gmail.com>
2018-12-08 14:24:01 -0500

    broken java not working

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 //Global Variables:
-var userImput = event.key;
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var rando = math.floor(math.random*26)
-var computerGuess ;
-var validate = -1;
+var rando = math.floor(math.random * 26)
+var computerGuess;
+var validate = '';
 var wins = 0;
 var lose = 0;
 var gusss = 0;
 var guessLeft = 9;
 
 //User Typing Letter:
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
+    var userGuess = event.key;
 
-//User Types letter:
+    //User Types letter:
+    alphabet.indexOf(userGuess) = validate
+     if(validate == -1){
+         alert("Invaild Key Option")
+     }
+         }
+
+    //Computer Guess 
+    computerGuess = alphabet[rando]
+
+
