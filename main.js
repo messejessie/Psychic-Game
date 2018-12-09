a4470b3fde273c132e3f829679d6216a146fd803
messejessie <messejessie77@gmail.com>
2018-12-09 13:32:46 -0500

    start of today

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Global Variables:
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var rando = math.floor(math.random()*26)
-var validate = '';
+var rando = Math.floor(Math.random()*26)
+var validate;
 var wins = 0;
 var lose = 0;
 var gusss = 0;
@@ -12,11 +12,12 @@ document.onkeyup = function (event) {
     var userGuess = event.key;
 
     //User Types letter:
-    alphabet.indexOf(userGuess) = validate
+    alphabet.indexOf(userGuess);
      if(validate == -1){
          alert("Invaild Key Option")
+         console.log(validate)
      }
-     console.log(validate)
+     
          }
 
     //Computer Guess 
